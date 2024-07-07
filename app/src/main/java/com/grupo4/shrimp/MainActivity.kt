ac2e89c505798cf127c0c2c19edd4edfb24fae36
Jesuarzm <jesuarzambrano@hotmail.com>
2024-07-07 16:15:28 -0600

    a

--- app/src/main/java/com/grupo4/shrimp/MainActivity.kt
+++ app/src/main/java/com/grupo4/shrimp/MainActivity.kt
@@ -16,32 +16,6 @@ import com.grupo4.shrimp.ui.theme.ShrimpTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ShrimpTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ShrimpTheme {
-        Greeting("Android")
+        setContentView(R.layout.registro)
     }
 }
